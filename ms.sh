b5e9f0430b6bab330fddf1f556bd4e3666d97019
davidly <davidly_lee@hotmail.com>
2025-01-08 16:21:58 -0800

    turn off bogus warning from g++

--- ms.sh
+++ ms.sh
@@ -1,2 +1,2 @@
-g++ -DARMOS -Ofast -Wno-psabi -fsigned-char -fno-builtin -I . armos.cxx arm64.cxx -o armos -static
+g++ -DARMOS -Ofast -Wno-psabi -Wno-stringop-overflow -fsigned-char -fno-builtin -I . armos.cxx arm64.cxx -o armos -static
 # cp armos /mnt/c/users/david/onedrive/armos/bin
