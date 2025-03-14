d949e34f26120e75c2ee46f3fd739703fcb8a9d4
davidly <davidly_lee@hotmail.com>
2025-03-14 14:27:43 -0700

    actually fix gnu c builds

--- mgr.bat
+++ mgr.bat
@@ -2,6 +2,6 @@
 setlocal
 
 rem compile with -O3 not -Ofast so NaN works
-g++ -Ofast -ggdb -fsigned-char -D ARMOS -D _MSC_VER armos.cxx arm64.cxx -I ../djl -D NDEBUG -o armosg.exe -static
+g++ -O3 -ggdb -fsigned-char -D ARMOS -D _MSC_VER armos.cxx arm64.cxx -I ../djl -D NDEBUG -o armosg.exe -static
 
 
