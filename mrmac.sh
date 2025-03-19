84f129eddcebee04a533fcf798b4f84ea9e2ace8
davidly <davidly_lee@hotmail.com>
2025-03-19 08:16:29 -0700

    build for macos with -O3, not -Ofast so NaN works

--- mrmac.sh
+++ mrmac.sh
@@ -1,2 +1,2 @@
-g++ -DARMOS -Ofast -DNDEBUG -fsigned-char -fno-builtin -I . armos.cxx arm64.cxx -o armos
+g++ -DARMOS -O3 -DNDEBUG -fsigned-char -fno-builtin -I . armos.cxx arm64.cxx -o armos
 # cp armos /mnt/c/users/david/onedrive/armos/bin
