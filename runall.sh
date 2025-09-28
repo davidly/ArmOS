df1ef390ed08a2fc57f835397052a5b565c669b7
davidly <davidly_lee@hotmail.com>
2025-09-28 10:16:17 -0700

    add trw2 and ff tests to automation

--- runall.sh
+++ runall.sh
@@ -22,7 +22,7 @@ outputfile="runall_test.txt"
 date_time=$(date)
 echo "$date_time" >$outputfile
 
-for arg in tcmp t e printint sieve simple tmuldiv tpi ts tarray tbits trw tmmap tstr \
+for arg in tcmp t e printint sieve simple tmuldiv tpi ts tarray tbits trw trw2 tmmap tstr \
            tdir fileops ttime tm glob tap tsimplef tphi tf ttt td terrno t_setjmp tex \
            mm tao pis ttypes nantst sleeptm tatomic lenum tregex trename nqueens;
 do
@@ -55,6 +55,15 @@ do
         $_armoscmd c_tests/bin$opt/ba -a:$codegen -x c_tests/tp.bas >>$outputfile
         $_armoscmd c_tests/clangbin$opt/ba -a:$codegen -x c_tests/tp.bas >>$outputfile
     done
+done
+
+echo test ff . ff.c
+for opt in 0 1 2 3 fast;
+do
+    echo test c_tests/bin$opt/ff . ff.c >>$outputfile
+    $_armoscmd c_tests/bin$opt/ff . ff.c >>$outputfile
+    echo test c_tests/clangbin$opt/ff . ff.c >>$outputfile
+    $_armoscmd c_tests/clangbin$opt/ff . ff.c >>$outputfile
 done
 
 for arg in e ttt fileops ato tap real tphi mysort tmm;
